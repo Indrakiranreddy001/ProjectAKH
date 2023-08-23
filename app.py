119902d68e07141d129eebed548a986215c9ced6
Indrakiranreddy001 <67328443+Indrakiranreddy001@users.noreply.github.com>
2023-08-23 08:55:03 +0530

    Add files via upload
    added files related to projectdata

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+import random
+import string
+
+app = Flask(__name__)
+
+@app.route('/generate-dataset', methods=['POST'])
+def generate_dataset():
+    data = request.json
+    features = data['features']
+    data_types = data['dataTypes']
+    sample_data = data['sampleData']
+
+    dataset = generate_synthetic_dataset(features, data_types, sample_data)
+
+    return jsonify(dataset)
+
+def generate_synthetic_dataset(features, data_types, sample_data):
+    dataset = []
+
+    for _ in range(sample_data):
+        data_row = {}
+
+        for feature, data_type in zip(features, data_types):
+            if data_type == 'string':
+                data_row[feature] = ''.join(random.choice(string.ascii_letters) for _ in range(10))
+            elif data_type == 'number':
+                data_row[feature] = random.randint(0, 100)
+            # Add more data types as needed
+
+            # Handle other data types here...
+
+        dataset.append(data_row)
+
+    return dataset
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ script.js
@@ -0,0 +1,33 @@
+const generateButton = document.getElementById('generateButton');
+const resultSection = document.getElementById('result');
+
+generateButton.addEventListener('click', () => {
+    const featuresInput = document.getElementById('features').value;
+    const dataTypesInput = document.getElementById('dataTypes').value;
+    const sampleDataInput = document.getElementById('sampleData').value;
+
+    const features = featuresInput.split(',').map(feature => feature.trim());
+    const dataTypes = dataTypesInput.split(',').map(dataType => dataType.trim());
+
+    const requestBody = {
+        features: features,
+        dataTypes: dataTypes,
+        sampleData: parseInt(sampleDataInput)
+    };
+
+    fetch('/generate-dataset', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+    .then(response => response.json())
+    .then(dataset => {
+        // Display the generated dataset
+        resultSection.innerHTML = JSON.stringify(dataset, null, 2);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
